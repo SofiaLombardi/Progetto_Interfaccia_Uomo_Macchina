2ddc10e09a6d7b21dcf5c2afa28129367b242af8
Sofia Lombardi <114483561+SofiaLombardi@users.noreply.github.com>
2025-01-07 16:46:23 +0100

    Delete Flower_App1 directory
    Rimozione directory superflua

--- Flower_App1/Infrastructure/LoginException.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Flower_App1.Infrastructure
-{
-    public class LoginException : Exception
-    {
-        public LoginException(string message) : base(message) { }
-    }
-}
